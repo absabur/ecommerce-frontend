c7dd815eb996588f4d38286cfdbc8e2bf3578273
Md Abdus Sabur <125259077+absabur@users.noreply.github.com>
2025-03-06 21:36:21 +0600

    Update BackendUrl.js

--- src/BackendUrl.js
+++ src/BackendUrl.js
@@ -1,3 +1,3 @@
-export const BackendUrl = "https://advisory-melosa-absabur-d1c74e70.koyeb.app";
+export const BackendUrl = "https://ecommerce-backend-ei1t.onrender.com";
 
 // export const BackendUrl = `http://localhost:6001`;
